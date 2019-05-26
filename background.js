acc2d580213811815fb6df88312f5e355184c21b
inwerpsel <pieter.vincent1988@gmail.com>
2019-05-26 23:23:15 +0200

    initial commit

---
+++ background.js
@@ -0,0 +1,141 @@
+// Search for a page to go to when a recognized import is clicked
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if ( request.message !== "import_clicked" ) {
+            return;
+        }
+
+        // Get composer.json
+
+        getComposerFiles(request.username, request.repository, request.branch, function (jsonJson, lockJson) {
+
+            let filename
+            let url
+            let folder
+
+            if (lockJson) {
+                console.log(jsonJson)
+                console.log(lockJson)
+                let allPackages = [ ...lockJson.packages, ...lockJson['packages-dev'] ];
+                allPackages.forEach( (package) => {
+                    if (
+                        typeof package.autoload === 'undefined'
+                        || typeof package.autoload['psr-4'] === 'undefined'
+                    ) {
+                        return;
+                    }
+                    for (packageNs in package.autoload['psr-4']) {
+                        // check if file fqcn starts with this namespace
+                        if (request.fqcn.startsWith(packageNs)) {
+                            console.log(package)
+
+                            folder = package.autoload['psr-4'][packageNs]
+                            filename = getFilenameFromFqcn(request.fqcn, packageNs)
+                            let baseUrl = package.source.url.replace(/\.git$/, '')
+                            url = `${baseUrl}/blob/master/${folder}/${filename}`
+                        }
+                    }
+                })
+            }
+
+            // Nothing in the packages so try this repo
+            if (!url) {
+                if (jsonJson) {
+                    if ('psr-4' in jsonJson.autoload) {
+                        Object.keys(jsonJson.autoload['psr-4']).forEach( (ns) => {
+                            console.log(ns)
+                            if (request.fqcn.startsWith(ns)) {
+                                folder = jsonJson.autoload['psr-4'][ns]
+                                filename = getFilenameFromFqcn(request.fqcn, ns)
+                            }
+                        })
+                    }
+                    if ('psr-4' in jsonJson['autoload-dev']) {
+                        Object.keys(jsonJson['autoload-dev']['psr-4']).forEach( (ns) => {
+                            if (request.fqcn.startsWith(ns)) {
+                                folder = jsonJson['autoload-dev'][ns]
+                                filename = getFilenameFromFqcn(request.fqcn, ns)
+                            }
+                        })
+                    }
+                } else {
+                    // still have a shot if there are no files
+                    folder = 'src'
+                    let defaultNamespace = '\\App'
+                    filename = getFilenameFromFqcn(request.fqcn, defaultNamespace)
+                }
+
+                if (filename) {
+                    url = `https://github.com/${request.username}/${request.repository}/blob/master/${folder}/${filename}`
+                }
+            }
+
+            if (url) {
+                chrome.tabs.create({"url": url});
+            } else {
+                console.log('no url could be found')
+            }
+        })
+
+
+    }
+);
+
+let composerCache = {}
+
+function getComposerFiles (username, repository, branch, callback) {
+    let cacheTag = username + repository + branch
+
+    if (cacheTag in composerCache) {
+        cache = composerCache[cacheTag]
+        console.log('got json from cache: ' + cacheTag)
+        callback(cache.jsonJson, cache.lockJson)
+        return
+    }
+
+    let jsonRawUrl = `https://raw.githubusercontent.com/${username}/${repository}/${branch}/composer.json`
+    let lockRawUrl = `https://raw.githubusercontent.com/${username}/${repository}/${branch}/composer.lock`
+
+    let jsonJson
+    let lockJson
+
+    console.log('fetching composer.json from github raw: ' + jsonRawUrl)
+    let jsonRequest = new XMLHttpRequest();
+    jsonRequest.open("GET", jsonRawUrl, true); // true for asynchronous 
+    jsonRequest.onload = function (e) {
+        console.log('fetched url: ' + jsonRawUrl)
+        if (jsonRequest.responseText === '404: Not Found\n') {
+            console.log('No composer file found at ' + jsonRawUrl)
+
+            composerCache[cacheTag] = {jsonJson: null, lockJson: null}
+
+            return
+        }
+        jsonJson = JSON.parse(jsonRequest.responseText)
+
+        lockRequest = new XMLHttpRequest();
+        lockRequest.open('GET', lockRawUrl, true);
+        lockRequest.onload = function (e) {
+            if (lockRequest.responseText === '404: Not Found\n') {
+                composerCache[cacheTag] = {jsonJson: jsonJson, lockJson: null}
+                return callback(jsonJson, null)
+            }
+
+            lockJson = JSON.parse(lockRequest.responseText)
+            composerCache[cacheTag] = {jsonJson: jsonJson,lockJson: lockJson}
+            callback(jsonJson, lockJson)
+        }
+        lockRequest.send(null)
+
+    }
+    jsonRequest.send(null)
+}
+
+function getFilenameFromFqcn(fqdn, ns) {
+    console.log(fqdn, ns)
+    fileOnly = fqdn.replace(ns, '')
+    withForwardSlashes = fileOnly.replace(/\\/g, '\/',)
+    console.log(withForwardSlashes)
+
+    return withForwardSlashes + '.php'
+}
---
+++ content.js
@@ -0,0 +1,71 @@
+class Import {
+    constructor(fqcn, containerElement) {
+        this.fqcn = fqcn;
+        this.containerElement = containerElement;
+    }
+
+}
+
+window.onload = function () {
+    let regex = /^https:\/\/github\.com\/([\w-_]+)\/([\w-_]+)\/blob\/([\w-_]+)\/(.*\.php)/
+
+    matches = window.location.href.match(regex)
+
+    if (matches === null) {
+        return
+    }
+
+    let username = matches[1]
+    let repository = matches[2]
+    let branch = matches[3]
+    let filename = matchMedia[4]
+
+    let allSpanSpans = [...document.querySelectorAll('.js-file-line-container span > span')]
+
+    // for now let not handle the case that the class is in the current namespace
+    // let currentFileNamespace = allSpanSpans.find( span => span.innerHTML === 'namespace').nextElementSibling.innerHTML
+
+    imports = allSpanSpans.reduce( (result, span) => {
+        // php specific for now
+        if (span.textContent !== 'use') {
+            return result;
+        }
+
+        let fqcn = span.nextElementSibling.innerHTML;
+        // for now do nothing with global classes or use functions
+        if (!fqcn.includes('\\')) {
+            return result;
+        }
+
+        // get container of full import statement
+        let container = span.parentNode;
+
+        result.push(new Import(fqcn, container));
+
+        return result;
+    }, []);
+
+    if (imports.length === 0) {
+        return;
+    }
+
+    imports.forEach( (anImport) => {
+        anImport.containerElement.setAttribute('title', 'nav available')
+
+        anImport.containerElement.style.cursor = 'pointer';
+  
+        // Add listener for middle click to search files
+        anImport.containerElement.addEventListener('click', (event) => {
+            chrome.runtime.sendMessage({
+                "message": "import_clicked", 
+                "fqcn": anImport.fqcn,
+                "username": username,
+                "repository": repository,
+                "branch": branch
+            })
+        })
+    
+    })
+
+    chrome.runtime.sendMessage({"message": "imports_found", "imports": imports})
+}
